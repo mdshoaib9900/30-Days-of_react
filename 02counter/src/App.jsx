8e8c67ad42937e36a7dd0096df03652a16b9de4e
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-08-09 00:13:10 +0530

    counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+   let [counter,setCounter] =useState('')
+
+   const addValue=()=>{
+    counter+=1;
+    setCounter(counter)
+    
+   }
+   const decValue=()=>{
+    counter-=1;
+    if(counter>=0){
+      setCounter(counter)
+    }else{
+      alert("please increase counter")
+    }
+   }
+
+  return (
+    <>
+    <h1>chai aur react</h1>
+    <h2>counter value :{counter}</h2>
+    <button
+    onClick={addValue}>Add value</button>
+    <button
+    onClick={decValue}>decrease value</button>
+    </>
+  )
+}
+
+export default App
+ 
