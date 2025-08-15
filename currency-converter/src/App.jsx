4fffdb09ff372e31e332e4d24458a7753213a7e7
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-08-15 22:32:23 +0530

    updated

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -34,9 +34,8 @@ function App() {
                 <div className=" w-full max-w-md mx-auto  border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
                     <form
                         onSubmit={(e) => {
-                            e.preventDefault();
-                            convert()
-                           
+                           e.preventDefault();
+                           convert();
                         }}
                     >
                         <div className="w-full mb-1  ">
