ea8208345a86f789b4227e51413055457dcd0035
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-08-25 21:35:16 +0530

    updated

--- react-couter/src/components/Github/Github.jsx
+++ react-couter/src/components/Github/Github.jsx
@@ -3,21 +3,21 @@ import { useLoaderData } from 'react-router-dom'
 
 function Github() {
     const data=useLoaderData()
-    // const [data,setData]=useState([])
-    // useEffect(()=>{
-    //     fetch('https://api.github.com/users/hiteshchoudhary')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         setData(data)
-    //     })
-    // },[])
+//     const [data,setData]=useState([])
+//     useEffect(()=>{
+//         fetch('https://api.github.com/users/hiteshchoudhary')
+//         .then(response => response.json())
+//         .then(data => {
+//             setData(data)
+//         })
+//     },[])
   return (
     <div className='text-center m-4 bg-gray-600 text-white
     p-4 text-3xl '>Github followers : {data?.followers}
     <img className='justify-center align-center'
      src={data?.avatar_url} alt="git Picture" width={300} /></div>
   )
-}
+ }
 
 export default Github
 
